0a21ba63209f2718e7c8d437e4f0c06c1edbc99d
Jonathan Reem <jonathan.reem@gmail.com>
2014-04-22 22:31:17 -0700

    Added fields in the default ui.

--- ui.js
+++ ui.js
@@ -6,12 +6,23 @@ $(document).ready(function () {
 
   var timeStream = new TimeStream(canvas, ctx);
   timeStream.addEmitter(
-    new Emitter(new Vector(100, 230), Vector.fromAngle(10, 0)));
+    new Emitter(new Vector(100, 230), Vector.fromAngle(2, 0)));
   timeStream.addEmitter(
-    new Emitter(new Vector(100, 430), Vector.fromAngle(10, 0)));
+    new Emitter(new Vector(100, 430), Vector.fromAngle(5, 0)));
   timeStream.addEmitter(
-    new Emitter(new Vector(100, 630), Vector.fromAngle(10, 0)));
+    new Emitter(new Vector(100, 630), Vector.fromAngle(5, 0)));
   timeStream.addEmitter(
-    new Emitter(new Vector(100, 830), Vector.fromAngle(10, 0)));
+    new Emitter(new Vector(900, 630), Vector.fromAngle(2, -Math.PI)));
+
+  timeStream.addField(
+    new Field(new Vector(600, 430), -400));
+
+  timeStream.addField(
+    new Field(new Vector(600, 630), 200));
+
+  timeStream.addField(
+    new Field(new Vector(600, 230), 800));
+
+
   timeStream.run();
 });
